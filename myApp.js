52bd15d6377af9142d5ea39305ff55f7ccf91774
Ayub Khoirudin <khoirudinayub971@gmail.com>
2024-11-21 13:10:32 +0700

    change some file

--- myApp.js
+++ myApp.js
@@ -1,39 +1,53 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
+let bodyParser = require("body-parser");
+
 let app = express();
 
+const test = (req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+};
 
+app.use("/public", express.static(__dirname + "/public"));
+app.use(test);
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({ message: "HELLO JSON" });
+	} else {
+		res.json({ message: "Hello json" });
+	}
+});
 
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.json({ time: req.time });
+	}
+);
 
+app.get("/:word/echo", (req, res) => {
+	res.json({ echo: req.params.word });
+});
 
+app.get("/name", (req, res) => {
+	const data = req.query;
+	res.json({ name: `${data.first} ${data.last}` });
+});
 
+app.post("/name", (req, res) => {
+	const data = req.body;
+	res.json({ name: `${data.first} ${data.last}` });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
